31bfa7e4ea8787dc746d4a520bc0f5341ace0b84
Kaktoose <72473270+Kaktoose@users.noreply.github.com>
2021-06-21 18:51:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,33 @@
+const fs = require('fs');
 const { ViteAPI } = require('@vite/vitejs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+require('dotenv').config(); 
+client.commands = mew Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 
- //declaring messages in variables
-  const msg1 = ("You begged Elon for digicoins. You received" + vite_amount + "Vite to the moon.")
-  const msg2 = ("You're walking outside and Vite falls from the sky" + vite_amount)
-  const msg3 = ("You run into a wild Shiba Inu, he gifts you the almighty Vite token. You received" + vite_amount)
-
-
-
-client.on('start', () =>{
-console.log("vite faucet is online")
-
-
-})
-
-// for help
-client.on('message', message =>{
-if(message === "*help"){
-  message.channel.send("")
-
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
 }
-})
 
-//for demanding vite
-client.on('message', message =>{
-  //find user in database, if not found, don't continue
-  
-
-
-
-//putting options into an array
-  var msgs = [msg1, msg2, msg3];
-
-//if demand
-if(message === "*demand"){
-
-//random message
-var msg = msgs[Math.floor(Math.random() * msgs.length)];
-
-//random viteValue
-let faucetValue = Math.random();
-let vite_amount = faucetValue;
-//find user address
-
-
-
-//send user vite_amount
- 
-
-
-
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args, client) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args, client) => event.execute(...args));
+	}
 }
 
 
 
-})
+
+
+
