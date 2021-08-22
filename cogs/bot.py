7bff3f047477c8edd705b16b6561287734fdb409
JohnGarnham <john.e.garnham@gmail.com>
2021-08-22 01:34:34 -0600

    Show bot config as embed Close #24

--- cogs/bot.py
+++ cogs/bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import dotenv
 from common import Common
@@ -12,7 +13,7 @@ class BotCog(commands.Cog, name="Bot"):
     async def set_prefix(self, ctx, new_prefix=""):
         # Check that new prefix is valid
         if(new_prefix == ""):
-            await ctx.reply(f"Usage: {self.bot.command_prefix}set_prefix <new_prefix>")
+            await ctx.send(f"Usage: {self.bot.command_prefix}set_prefix <new_prefix>")
             return
         try:
             # Update command prefix
@@ -22,6 +23,7 @@ class BotCog(commands.Cog, name="Bot"):
             # Update the bot status
             await self.bot.update_status()
             # Alert user of successful command prefix update
+            Common.log(f"{ctx.message.author} set new command prefix to \"{new_prefix}\"")
             await ctx.send(f"Set new command prefix to \"{new_prefix}\"")
         except Exception as e:
             raise Exception(f"Could not change command prefix to \"{new_prefix}\"", e)  
@@ -30,23 +32,24 @@ class BotCog(commands.Cog, name="Bot"):
     @commands.command(name='show_config', aliases=['config','botconfig','bot_config'], help="Show the current bot config")
     async def show_config(self,ctx):
         try:
-            # Show ALL information
-            response = f"**Disabled:** {self.bot.disabled}" + \
-                f"\n**Command Prefix:** {self.bot.command_prefix}" + \
-                f"\n**Logging Level:** {self.bot.logging_level}" + \
-                f"\n**RPC URL:** {self.bot.rpc_url}" + \
-                f"\n**Faucet Address:** {self.bot.faucet_address}" + \
-                f"\n**Answer Timeout:** {self.bot.answer_timeout}" + \
-                f"\n**Greylist Duration in Minutes:** {self.bot.greylist_duration}" + \
-                f"\n**Token Type ID (TTI):** {self.bot.token_id}" + \
-                f"\n**Token Amount Per Correct Answer:** {self.bot.token_amount}" + \
-                f"\n**Max Rewards Per Time Period:** {self.bot.max_rewards_amount}"
-            await ctx.send(response)
+            # Show config info as embed
+            embed=discord.Embed(title="Config", description="Current bot configuration", color=discord.Color.orange())
+            embed.add_field(name="Disabled", value=self.bot.disabled, inline=True)
+            embed.add_field(name="Command Prefix", value=self.bot.command_prefix, inline=True)
+            embed.add_field(name="Logging Level", value=self.bot.logging_level, inline=True)
+            embed.add_field(name="RPC URL", value=self.bot.rpc_url, inline=True)
+            embed.add_field(name="Faucet Address", value=self.bot.faucet_address, inline=True)
+            embed.add_field(name="Token Type ID (TTI)", value=self.bot.token_id, inline=True)
+            embed.add_field(name="Token Reward per Correct Answer", value=self.bot.token_amount, inline=True)
+            embed.add_field(name="Answer Timeout [Seconds]", value=self.bot.answer_timeout, inline=True)
+            embed.add_field(name="Max Rewards per Quiz Period", value=self.bot.max_rewards_amount, inline=True)
+            embed.add_field(name="Quiz Period [Minutes]", value=self.bot.greylist_duration, inline=True)
+            await ctx.send(embed=embed)
         except Exception as e:
             raise Exception("Exception showing info summary", e)   
 
     # Set logging level for the bot
-    @commands.command(name='set_logging', help="Set logging level [Admin Only]")
+    @commands.command(name='set_logging', aliases=['set_logging_level'], help="Set logging level [Admin Only]")
     @commands.has_any_role('Core','Dev')
     async def set_logging(self,ctx,new_level):
         try:
@@ -57,7 +60,8 @@ class BotCog(commands.Cog, name="Bot"):
             # Save in .env file
             dotenv.set_key(".env","logging_level", new_level)
             # Report successful logging level update to user
-            await ctx.reply(f"Set logging level to {new_logging_level}")
+            Common.log(f"{ctx.message.author} set new logging level to \"{new_logging_level}\"")
+            await ctx.send(f"Set logging level to {new_logging_level}")
         except Exception as e:
             raise Exception(f"Could not change logging level to {new_logging_level}", e)    
 
@@ -67,14 +71,15 @@ class BotCog(commands.Cog, name="Bot"):
         try:
             new_greylist = float(param)
             if(new_greylist < 0):
-                await ctx.reply(f"Greylist duration must be positive")
+                await ctx.send(f"Greylist duration must be positive")
                 return
             self.bot.greylist_duration = new_greylist
             # Save in .env file
             dotenv.set_key(".env","greylist_duration", param)
-            await ctx.reply(f"Greylist time period has been updated to {self.bot.greylist_duration} minutes")
+            Common.log(f"{ctx.message.author} set new greylist time to \"{new_greylist}\"")
+            await ctx.send(f"Greylist time period has been updated to {self.bot.greylist_duration} minutes")
         except ValueError:
-            await ctx.reply(f"Greylist duration must be a valid number")
+            await ctx.send(f"Greylist duration must be a valid number")
             return
         except Exception as e:
             Common.logger.error(f"Error in set_greylist {e}", exc_info=True)   
@@ -86,17 +91,18 @@ class BotCog(commands.Cog, name="Bot"):
         try:
             new_token_amount = float(param)
             if(new_token_amount < 0):
-                await ctx.reply(f"Token reward must be positive")
+                await ctx.send(f"Token reward must be positive")
                 return
             if(new_token_amount >= self.bot.max_rewards_amount):
-                await ctx.reply(f"Max_rewards amount must be greater than token_reward amount")
+                await ctx.send(f"Max_rewards amount must be greater than token_reward amount")
                 return
             self.bot.token_amount = new_token_amount
             # Save in .env file
             dotenv.set_key(".env","token_amount", param)
-            await ctx.reply(f"Token reward has been updated to {self.bot.token_amount}")
+            Common.log(f"{ctx.message.author} set new token reward amount to \"{new_token_amount}\"")
+            await ctx.send(f"Token reward has been updated to {self.bot.token_amount}")
         except ValueError:
-            await ctx.reply(f"Token reward be a valid number")
+            await ctx.send(f"Token reward be a valid number")
             return
         except Exception as e:
             Common.logger.error(f"Error in set_token_reward {e}", exc_info=True)   
@@ -108,17 +114,18 @@ class BotCog(commands.Cog, name="Bot"):
         try:
             amount = float(param)
             if(amount < 0):
-                await ctx.reply(f"Max rewards amount must be positive")
+                await ctx.send(f"Max rewards amount must be positive")
                 return
             if(amount <= self.bot.token_amount):
-                await ctx.reply(f"Max_rewards amount must be greater than token_reward amount")
+                await ctx.send(f"Max_rewards amount must be greater than token_reward amount")
                 return
             self.bot.max_rewards_amount = amount
             # Save in .env file
             dotenv.set_key(".env","max_rewards_amount",param)
-            await ctx.reply(f"Max rewards amount has been updated to {self.bot.max_rewards_amount}")
+            Common.log(f"{ctx.message.author} set new max rewards amount to \"{amount}\"")
+            await ctx.send(f"Max rewards amount has been updated to {self.bot.max_rewards_amount}")
         except ValueError:
-            await ctx.reply(f"Maximum reward amount must be a valid number")
+            await ctx.send(f"Maximum reward amount must be a valid number")
             return
         except Exception as e:
             Common.logger.error(f"Error in set_max_reward {e}", exc_info=True)   
@@ -129,14 +136,14 @@ class BotCog(commands.Cog, name="Bot"):
     @commands.has_any_role('Core','Dev')
     async def start(self,ctx):
         self.bot.disabled = False
-        await ctx.reply("Trivia game has been enabled")
+        await ctx.send("Trivia game has been enabled")
 
     # Start the bot
     @commands.command(name='stop', help="Stop the bot [Admin Only]")
     @commands.has_any_role('Core','Dev')
     async def stop(self,ctx):
         self.bot.disabled = True     
-        await ctx.reply(f"Trivia game has been disabled") 
+        await ctx.send(f"Trivia game has been disabled") 
 
     @commands.command(name='invite', help="Displays invite link [Admin Only]")
     @commands.has_any_role('Core','Dev')
